26943052472c6c742571f2b8cd8905d1c21e3770
Arcusterrr <nikita.kargapolov000@gmail.com>
2021-01-30 21:25:05 +0500

    Добавьте файлы проекта.

---
+++ Game2048/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+        }
+
+        void Start()
+        {
+            Model model = new Model(4);
+            model.Start();
+            while (true)
+            {
+                Show();
+                switch (Console.ReadKey(false).Key)
+                {
+                    case ConsoleKey.LeftArrow:  model.Left(); break;
+                    case ConsoleKey.RightArrow: model.Right(); break;
+                    case ConsoleKey.UpArrow:    model.Up(); break;
+                    case ConsoleKey.DownArrow:  model.Down(); break;
+                    case ConsoleKey.Escape:     model.Start(); break;
+                }
+            }
+        }
+
+        void Show(Model model)
+        {
+
+        }
+    }
+}
